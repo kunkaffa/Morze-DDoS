c06e1bde278c5a05e8edbdd6ac0bff2b48e23150
kunkaffa <kunkaffa3@gmail.com>
2025-09-04 17:41:07 +0700

    Update morze-ddos.py

--- morze-ddos.py
+++ morze-ddos.py
@@ -77,7 +77,7 @@ def main():
     bar.finish()
 
     while True:
-        print(("\033[32m[÷]\033[31m▒▒\033[92m▒▒BUILD\033[33m▒▒ATTACK\033[31m▒▒FLOODING▒▒\033[37mTARGET▒▒".format(len(socket_list))))
+        print(("\033[32m[÷]\033[31m▒▒\033[92m▒▒MORZE\033[33m▒▒REQUEST\033[31m▒▒SENT▒▒\033[37m▒▒".format(len(socket_list))))
 
         for s in socket_list:
             try:
